1fbde5884948ea1dc5adaff7913ba266e3fc736a
luzardo_fonseca <luzardo.fonseca@hotmail.com>
2022-05-16 07:42:58 -0300

    movistars

--- frontend/src/pages/listing/index.tsx
+++ frontend/src/pages/listing/index.tsx
@@ -1,10 +1,15 @@
 import './styles.css';
 import Pagination from "../../components/pagination";
+import MovieStars from 'components/moviestars';
 
-function listing(){
-    
-    return(
-       <Pagination />
+function listing() {
+
+    return (
+        <>
+            <Pagination />
+            <MovieStars />
+
+        </>
     );
 }
 export default listing;
